84ce712ad2d83a55434ee51877e47ec710e499b4
VichetN <vichet.noeng66@gmail.com>
2022-08-30 17:30:53 +0700

    vvv

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import React, { useEffect, useRef, useState } from "react";
 import * as faceapi from 'face-api.js';
 import { Camera } from 'react-cam';
 
-function capture(imgSrc) {
-  console.log(imgSrc);
+async function loadLabeledImages() {
+
+  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark','Vichet']
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = []
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+        descriptions.push(detections.descriptor)
+      }
+
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    })
+  )
+
 }
 
 function App() {
 
   const [modelsLoaded, setModelsLoaded] = useState(false);
   const [captureVideo, setCaptureVideo] = useState(false);
-
-  const cam = useRef(null);
+  const [faces, setFaces] = useState(null)
 
   const videoRef = useRef();
   const videoHeight = 480;
@@ -21,6 +34,7 @@ function App() {
   const canvasRef = useRef();
 
   useEffect(() => {
+
     const loadModels = async () => {
       const MODEL_URL = process.env.PUBLIC_URL + '/models';
 
@@ -29,10 +43,38 @@ function App() {
         faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
         faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
         faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
-      ]).then(setModelsLoaded(true));
+        faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL),
+      ]).then(async () => {
+        setModelsLoaded(true)
+
+        // const faceDescriptor = await loadLabeledImages();
+
+        // if (faceDescriptor) {
+        //   setFaces(faceDescriptor)
+        // }
+
+      });
     }
+
+
     loadModels();
+
   }, []);
+
+  const loadLabel = async () => {
+    const faceDescriptor = await loadLabeledImages();
+    const faces = await faceapi?.FaceMatcher(faceDescriptor, 0.6);
+
+    if (faceDescriptor) {
+      setFaces(faceDescriptor)
+    }
+  }
+
+  // useEffect(() => {
+  //   if (modelsLoaded) {
+  //     loadLabel()
+  //   }
+  // }, [loadLabel, modelsLoaded]);
 
   const startVideo = () => {
     setCaptureVideo(true);
@@ -48,27 +90,39 @@ function App() {
       });
   }
 
-  const handleVideoOnPlay = () => {
+  const handleVideoOnPlay = async () => {
+    const faceDescriptor = await loadLabeledImages();
+    const faceMatcher = new faceapi.FaceMatcher(faceDescriptor, 0.6);
     setInterval(async () => {
       if (canvasRef && canvasRef.current) {
         canvasRef.current.innerHTML = faceapi?.createCanvasFromMedia(videoRef.current);
         const displaySize = {
           width: videoWidth / 1.9,
-          height: videoHeight 
+          height: videoHeight
         }
 
         faceapi.matchDimensions(canvasRef.current, displaySize);
 
-        const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-
+        const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withFaceDescriptors();
         const resizedDetections = faceapi.resizeResults(detections, displaySize);
 
-        canvasRef && canvasRef.current && canvasRef.current.getContext('2d').clearRect(0, 0, videoWidth, videoHeight);
-        canvasRef && canvasRef.current && faceapi.draw.drawDetections(canvasRef.current, resizedDetections);
-        canvasRef && canvasRef.current && faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
-        canvasRef && canvasRef.current && faceapi.draw.drawFaceExpressions(canvasRef.current, resizedDetections);
+        // console.log(detections)
+
+        const results = resizedDetections?.map(d => faceMatcher.findBestMatch(d.descriptor))
+        console.log(faceMatcher,'tes')
+        results?.forEach((result, index) => {
+          const box = resizedDetections[index].detection?.box
+          const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+          drawBox?.draw(canvasRef.current)
+        })
+
+        // canvasRef && canvasRef.current && canvasRef.current.getContext('2d').clearRect(0, 0, videoWidth, videoHeight);
+        // canvasRef && canvasRef.current && faceapi.draw.drawDetections(canvasRef.current, resizedDetections);
+
+        // canvasRef && canvasRef.current && faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
+        // canvasRef && canvasRef.current && faceapi.draw.drawFaceExpressions(canvasRef.current, resizedDetections);
       }
-    }, 10)
+    }, 100)
   }
 
   const closeWebcam = () => {
@@ -95,7 +149,7 @@ function App() {
         captureVideo ?
           modelsLoaded ?
             <>
-              <div style={{ display: 'flex', justifyContent: 'center', overflow:'hidden',width:'100%',position:'relative' }}>
+              <div style={{ display: 'flex', justifyContent: 'center', overflow: 'hidden', width: '100%', position: 'relative' }}>
                 {/* <Camera
                   showFocus={true}
                   front={false}
